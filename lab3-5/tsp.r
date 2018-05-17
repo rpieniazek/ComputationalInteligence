aa1b8e1bd70fc74d04ce09fcb411f7e2d8ce60a0
evelan <evelan3094@gmail.com>
2018-05-17 12:24:54 +0200

    tsp working example

--- lab3-5/tsp.r
+++ lab3-5/tsp.r
@@ -1,12 +1,12 @@
 #https://cran.r-project.org/web/packages/GA/vignettes/GA.html
 #install.packages("GA");# do instalacji biblioteki GA
 #install.packages("globalOptTests")
-#install.packages("TSPLIB")
+#install.packages("TSP")
 library(GA)
 library(globalOptTests)
 library(TSP)
 
-drill <-read_TSPLIB("~/Documents/bays29.tsp")
+drill <- read_TSPLIB("~/Documents/bays29.tsp")
 D <- as.matrix(drill)
 
 # given a tour, calculate the total distance
@@ -15,41 +15,33 @@ tourLength <- function(tour, distMatrix) {
   route <- embed(tour, 2)[, 2:1]
   sum(distMatrix[route])
 }
+
 # inverse of thetotal distance is the fitness
 tpsFitness <- function(tour, ...) 1/tourLength(tour, ...)
 
-
-getAdj <- function(tour) {
-  n <- length(tour)
-  from <- tour[1:(n - 1)]
-  to <- tour[2:n]
-  m <- n - 1
-  A <- matrix(0, m, m)
-  A[cbind(from, to)] <- 1
-  A <- A + t(A)
-  return(A)
-}
-
 # 2-d coordinates
-mds <- cmdscale(eurodist)
+mds <- cmdscale(D)
 x <- mds[, 1]
 y <- -mds[, 2]
 n <- length(x)
 
 B <- 100
 fitnessMat <- matrix(0, B, 2)
-A <- matrix(0, n, n)
+
+GA.fit <- ga(type = "permutation", fitness = tpsFitness, distMatrix = D, 
+             min = 1, max = nrow(D), popSize = 10, maxiter = 50, run = 100, 
+             keepBest=TRUE,
+             pmutation = 0.2, monitor = NULL)
+
 for (b in seq(1, B)) {
   # run a GA algorithm
   GA.rep <- ga(type = "permutation", fitness = tpsFitness, distMatrix = D, 
-               min = 1, max = attr(eurodist, "Size"), popSize = 10, maxiter = 50, run = 100, 
+               min = 1, max = nrow(D), popSize = 10, maxiter = 50, run = 100, 
+               keepBest=TRUE,
                pmutation = 0.2, monitor = NULL)
   
-  tour <- GA.rep@solution[1, ]
-  tour <- c(tour, tour[1])
-  fitnessMat[b, 1] <- GA.rep@best[GA.rep@iter]
-  fitnessMat[b, 2] <- GA.rep@mean[GA.rep@iter]
-  A <- A + getAdj(tour)
+  fitnessMat[b, 1] <- GA.rep@summary[GA.rep@iter]
+  fitnessMat[b, 2] <- GA.rep@summary[GA.rep@iter]
 }
 
 
